bab7409d2aad5d139a5910e654fd111a08bc9eff
wdr <2530869295@qq.com>
2026-05-04 17:04:27 +0800

    littcode5.4

---
+++ leetcode/Linked List/desgin-Linked List.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<climits>
+#include<algorithm>
+
+class MyLinkedList {
+public:
+    MyLinkedList() {
+
+    }
+
+    int get(int index) {
+
+    }
+
+    void addAtHead(int val) {
+
+    }
+
+    void addAtTail(int val) {
+
+    }
+
+    void addAtIndex(int index, int val) {
+
+    }
+
+    void deleteAtIndex(int index) {
+
+    }
+};
